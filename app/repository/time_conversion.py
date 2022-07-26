aed64cf81742deff82358a8e32f9822bb8f2a6ec
Puneet Udhayan <48212981+PuneetUdhayan@users.noreply.github.com>
2022-07-26 21:45:09 +0530

    Database models created
    Created database models.
    Added a setup script to create and set up database.
    Added a class to convert time to integer that represent minutes from 12 am

---
+++ app/repository/time_conversion.py
@@ -0,0 +1,9 @@
+class TimeConversions():
+
+    @staticmethod
+    def mintues_to_time(time:str) -> str:
+        pass
+
+    @staticmethod
+    def time_to_minutes(mintues:int) -> int:
+        pass
