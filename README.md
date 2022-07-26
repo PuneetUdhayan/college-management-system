9450c157f3c260a37f249717393913fbeb880853
Puneet Udhayan <48212981+PuneetUdhayan@users.noreply.github.com>
2022-07-26 22:27:22 +0530

    TimeConversion class implemented
    TimeConversion class implemented and tests passed.

--- README.md
+++ README.md
@@ -11,8 +11,20 @@ To run the tests use the command:
 python -m pytest tests
 ```
 
-To generate coverage repore run the following command:
+To generate coverage run the following command:
 
 ```
-python -m pytest --cov=testcoverage tests
+python -m coverage run -m pytest tests
+```
+
+To output the report for the previously run coverage tests:
+
+```
+coverage report --omit=<environment_folder_name>/*
+```
+
+To export the report in HTML:
+
+```
+coverage html --omit=<environment_folder_name>/*
 ```
--- app/repository/time_conversion.py
+++ app/repository/time_conversion.py
@@ -1,9 +1,44 @@
+class IncorrectTimeFormat(Exception):
+
+    def __init__(self,message):
+        super().__init__(message)
+
+
+class IncorrectMinutesValue(Exception):
+
+    def __init__(self,message = "Minutes value must be between 0 and 1,440"):
+        super().__init__(message)
+
+
 class TimeConversions():
+    """TimeConverstion class is used to convert time from HH:MM format
+    to an integer reprsenting minutes from midnight and vice versa
+    """
 
     @staticmethod
-    def mintues_to_time(time:str) -> str:
-        pass
+    def time_to_minutes(time:str) -> str:
+
+        timedata = time.split(':')
+
+        try:
+            hour = int(timedata[0])
+            if hour > 24:
+                raise Exception()
+            minutes = int(timedata[1])
+            if minutes > 60:
+                raise Exception()
+            return (hour*60)+minutes
+        except Exception as e:
+            raise IncorrectTimeFormat(str(e))
+
 
     @staticmethod
-    def time_to_minutes(mintues:int) -> int:
-        pass
+    def minutes_to_time(minutes:int) -> int:
+
+        if minutes < 0 or minutes > 1440:
+            raise IncorrectMinutesValue()
+        
+        hour = minutes//60
+        minutes = minutes%60
+
+        return f"{hour}:{minutes}"
